7e20855170fb19b4310ef4f75227fdf80aa1963d
Jaroslav <cebreus@live.com>
2022-02-14 11:05:03 +0100

    fac-api--env-dev.spec.js

--- cypress/integration/fac-dev/fac-api--env-dev.spec.js
+++ cypress/integration/fac-dev/fac-api--env-dev.spec.js
@@ -1,58 +1,69 @@
-describe('Test FAC developers', () => {
+describe('Test FAC API', () => {
 
-	it('Visits API home', () => {
-		cy.visit('https://apidev.fastandcomfy.io')
-	})
+  let email = 'me@cebre.us';
 
-	it('Check all links on FAC home', () => {
-		cy.visit('https://apidev.fastandcomfy.io')
-		cy.get('a:not([href^="mailto:"]):not([href*="www.linkedin.com"])').each(page => {
-			cy.request(page.prop('href'))
-		})
-	});
+  it('Visits API home', () => {
+    cy.visit('https://apidev.fastandcomfy.io')
+  })
 
-	it('Link: Read our docs', () => {
-		cy
-			.get('a')
-			.contains('Read our docs')
-			.should('be.visible')
-			.should('have.class', 'btn-secondary')
-			.should('have.attr', 'href', 'https://developersdev.fastandcomfy.io/swagger')
-	})
+  it('Check all links on API home', () => {
+    cy.visit('https://apidev.fastandcomfy.io')
+    cy.get('a:not([href^="mailto:"]):not([href*="www.linkedin.com"])').each(page => {
+      cy.request(page.prop('href'))
+    })
+  });
 
-	it('Link: Get your API key', () => {
-		cy
-			.get('a')
-			.contains('Get your API key')
-			.should('be.visible')
-			.should('have.class', 'btn-secondary')
-			.should('have.attr', 'href', '/apikey')
-	})
+  it('Link: Read our docs', () => {
+    cy
+      .get('a')
+      .contains('Read our docs')
+      .should('be.visible')
+      .should('have.class', 'btn-secondary')
+      .should('have.attr', 'href', 'https://developersdev.fastandcomfy.io/swagger')
+  })
 
-	it('Visits Get your API key', () => {
-		cy.visit('https://apidev.fastandcomfy.io/apikey')
-	})
+  it('Link: Get your API key', () => {
+    cy
+      .get('a')
+      .contains('Get your API key')
+      .should('be.visible')
+      .should('have.class', 'btn-secondary')
+      .should('have.attr', 'href', '/apikey')
+  })
 
-	// it('Send form', () => {
-	// 	cy
-	// 		.get('[data-cy="FormContact"]')
-	// 		.should('be.visible')
-	// 		.get('#email').then(($input) => {
-	// 			cy.wrap($input).should('have.attr', 'required')
-	// 		})
-	// 		.get('.btn-success').then(($btn) => {
-	// 			cy.wrap($btn).should('be.visible').should('contain', 'Submit').should('have.attr', 'disabled')
-	// 		})
-	// 		.wait(3000)
-	// 		.get('#email').then(($input) => {
-	// 			cy.wrap($input).focus().type('me@cebre.us')
-	// 		})
-	// 		.get('.btn-success').then(($btn) => {
-	// 			cy.wrap($btn).not('disabled')
-	// 			cy.wrap($btn).click()
-	// 		})
-	// 		.wait(3000)
-	// 		.get('[data-cy="Alert"].alert-success').should('be.visible').should('contain', 'Thank you for your message!')
-	// })
+  it('Visits Get your API key', () => {
+    cy.visit('https://apidev.fastandcomfy.io/apikey')
+  })
+
+  it('Send form', () => {
+    cy
+      .get('[data-cy="FormGetApiKey"]')
+      .should('be.visible')
+      .get('#email').then(($input) => {
+        cy.wrap($input)
+          .should('have.attr', 'required')
+      })
+      .get('button[type="submit"]').then(($btn) => {
+        cy.wrap($btn)
+          .should('be.visible')
+          .should('contain', 'Submit')
+        // .should('have.attr', 'disabled')
+      })
+      .wait(3000)
+      .get('#email').then(($input) => {
+        cy.wrap($input)
+          .focus()
+          .type(email)
+      })
+      .get('button[type="submit"]').then(($btn) => {
+        cy.wrap($btn).not('disabled')
+        cy.wrap($btn).click()
+      })
+      .wait(3000)
+      .get('[data-cy="FormVerifyApiKey"]')
+      .should('be.visible')
+      .get('[data-cy="HeaderPerex"]')
+      .should('contain', email)
+  })
 
 })
